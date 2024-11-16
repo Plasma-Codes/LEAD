fe56ebc4abb5bd38b3b49056df5b02cb2912569b
Plasma-Codes <plasmacodes001@gmail.com>
2024-11-16 21:52:36 +0300

    Add files via upload

---
+++ products.js
@@ -0,0 +1,60 @@
+const products = [
+    {
+        "id": 1,
+        "name":" LD01 i Phone",
+        "price": 200,
+        "image": "tunda_2-removebg-preview.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum, aliquid est cupiditate quidem atque laudantium, obcaecati corporis veniam explicabo nostrum, ea nisi maxime sequi. Dolorum quasi dolor dicta. Accusantium mollitia illum ratione, beatae ipsam quos reiciendis dolorem consequuntur aspernatur reprehenderit. "
+    },
+    {
+        "id": 2,
+        "name":" LD02 i Phone",
+        "price": 250,
+        "image": "tunda_4-removebg-preview.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum, aliquid est cupiditate quidem atque laudantium, obcaecati corporis veniam explicabo nostrum, ea nisi maxime sequi. Dolorum quasi dolor dicta. Accusantium mollitia illum ratione, beatae ipsam quos reiciendis dolorem consequuntur aspernatur reprehenderit. "
+    },
+    {
+        "id": 3,
+        "name":" LD03 i Phone",
+        "price": 290,
+        "image": "tunda_7-removebg-preview.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum, aliquid est cupiditate quidem atque laudantium, obcaecati corporis veniam explicabo nostrum, ea nisi maxime sequi. Dolorum quasi dolor dicta. Accusantium mollitia illum ratione, beatae ipsam quos reiciendis dolorem consequuntur aspernatur reprehenderit. "
+    },
+    {
+        "id": 4,
+        "name":" LD04 i Phone",
+        "price": 200,
+        "image": "tunda_8-removebg-preview.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum, aliquid est cupiditate quidem atque laudantium, obcaecati corporis veniam explicabo nostrum, ea nisi maxime sequi. Dolorum quasi dolor dicta. Accusantium mollitia illum ratione, beatae ipsam quos reiciendis dolorem consequuntur aspernatur reprehenderit. "
+    },
+    {
+        "id": 5,
+        "name":" LD05 i Phone",
+        "price": 300,
+        "image": "tunda_2-removebg-preview.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum, aliquid est cupiditate quidem atque laudantium, obcaecati corporis veniam explicabo nostrum, ea nisi maxime sequi. Dolorum quasi dolor dicta. Accusantium mollitia illum ratione, beatae ipsam quos reiciendis dolorem consequuntur aspernatur reprehenderit. "
+    },
+    {
+        "id": 6,
+        "name":" LD06 i Phone",
+        "price": 200,
+        "image": "tunda_4-removebg-preview.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum, aliquid est cupiditate quidem atque laudantium, obcaecati corporis veniam explicabo nostrum, ea nisi maxime sequi. Dolorum quasi dolor dicta. Accusantium mollitia illum ratione, beatae ipsam quos reiciendis dolorem consequuntur aspernatur reprehenderit. "
+    },
+    {
+        "id": 7,
+        "name":" LD07 i Phone",
+        "price": 200,
+        "image": "tunda_7-removebg-preview.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum, aliquid est cupiditate quidem atque laudantium, obcaecati corporis veniam explicabo nostrum, ea nisi maxime sequi. Dolorum quasi dolor dicta. Accusantium mollitia illum ratione, beatae ipsam quos reiciendis dolorem consequuntur aspernatur reprehenderit. "
+    },
+    {
+        "id": 8,
+        "name":" LD08 i Phone",
+        "price": 200,
+        "image": "tunda_8-removebg-preview.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum, aliquid est cupiditate quidem atque laudantium, obcaecati corporis veniam explicabo nostrum, ea nisi maxime sequi. Dolorum quasi dolor dicta. Accusantium mollitia illum ratione, beatae ipsam quos reiciendis dolorem consequuntur aspernatur reprehenderit. "
+    }
+
+];
+export default products;
